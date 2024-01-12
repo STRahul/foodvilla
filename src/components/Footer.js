ed64f9539beb36fd0379256d4155cab2c21dc152
rahul <rahultomar03187@gmail.com>
2024-01-12 11:49:18 +0530

    fix: footer ui

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 
 const Footer = ()=>{
     return(
-        <div className="text-center text-2xl">
+        <div className="text-center text-2xl border-t-2 fixed bottom-0 bg-white w-full">
             <h1>© 2023 FoodVilla. All rights reserved.</h1>
         </div>
     )
